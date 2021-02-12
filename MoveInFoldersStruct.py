a83531ca15ad9218d0fa13fd62581d33850460d7
albionasylaj <58147201+AlbionaSy@users.noreply.github.com>
2021-02-12 17:49:00 +0100

    Create MoveInFoldersStruct.py

---
+++ MoveInFoldersStruct.py
@@ -0,0 +1,20 @@
+import shutil
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+
+def CreateWidgets():
+    linkLabel = Label(root, text="SELECT THE FILE TO MOVE : ", bg="navajowhite3")
+    linkLabel.grid(row=1, column=0, pady=5, padx=5)
+
+    root.sourceText = Entry(root, width=50, textvariable=sourceLocation)
+    root.sourceText.grid(row=1, column=1, pady=5, padx=5, columnspan = 2)
+
+    source_browseButton = Button(root, text="BROWSE", command=SourceBrowse, width=15)
+    source_browseButton.grid(row=1, column=3, pady=5, padx=5)
+
+    destinationLabel = Label(root, text="SELECT THE DESTINATION : ", bg="navajowhite3")
+    destinationLabel.grid(row=2, column=0, pady=5, padx=5)
+
+    root.destinationText = Entry(root, width=50, textvariable=destinationLocation)
+    root.destinationText.grid(row=2, column=1, pady=5, padx=5, columnspan = 2)
